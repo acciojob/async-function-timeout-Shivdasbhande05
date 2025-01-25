f6440b4dd43b4ecb074372fd553539936bebc25d
shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-25 23:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const inputText = document.querySelector("#text");
+const delay = document.querySelector("#delay");
+const btn = document.querySelector("#btn");
+
+const output = document.querySelector("#output");
+
+async function submit () {
+    let textInput = inputText.value;
+    let timeDelay = parseInt(delay.value);
+
+    //console.log(timeDelay);
+
+    let promise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(textInput);
+        },delay);
+    });
+
+    let result = await promise;
+
+    output.innerHTML = result;
+}
+
+btn.addEventListener("click", submit);
